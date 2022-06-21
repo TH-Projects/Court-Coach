602a7d809c4c7d4d06af48e3f3fcfe49ade2e6dd
TH-Projects <64643362+TH-Projects@users.noreply.github.com>
2022-06-21 12:58:11 +0200

    text size on buttons changed

--- CourtCoach/DataHandler.cs
+++ CourtCoach/DataHandler.cs
@@ -26,39 +26,39 @@ namespace CourtCoach
         {
             SessionData sd = new SessionData();
             sd.Sessions = data;
-            var file = await getDataFile(); //bekommen xml file
+            var file = await getDataFile();
             string fd;
             using (StringWriter writer = new Utf8StringWriter())
             {
-                sr.Serialize(writer, sd); //serialisieren von den objekten in xml
+                sr.Serialize(writer, sd);                                   //serialisieren von den objekten in xml
                 fd = writer.ToString();
             }
-            await FileIO.WriteTextAsync(file, fd); //Schreibt in die datei
+            await FileIO.WriteTextAsync(file, fd);                          //Schreibt in die Datei
 
         }
         private async Task<StorageFile> getDataFile()
         {
-            StorageFolder storage = ApplicationData.Current.RoamingFolder; //späterer Speicherplatz holen
-            //await Launcher.LaunchFolderAsync(storage); //Ordner Starten (Explorer geht auf)
-            IStorageItem item = await storage.TryGetItemAsync("courtCoachData.xml"); // versuche element zu bekommen
-            if (item == null) //wenn element nicht existiert
+            StorageFolder storage = ApplicationData.Current.RoamingFolder;  //Speicherplatz holen/festlegen
+            //await Launcher.LaunchFolderAsync(storage);                    //Debugging Zeile
+            IStorageItem item = await storage.TryGetItemAsync("courtCoachData.xml"); //Versuche element zu bekommen
+            if (item == null)                                               //wenn Datei nicht vorhanden
             {
-                await storage.CreateFileAsync("courtCoachData.xml"); //datei erstellen          
+                await storage.CreateFileAsync("courtCoachData.xml");        //neue Datei erstellen          
             }
-            return await storage.GetFileAsync("courtCoachData.xml"); //rückgabe der existierenden Datei
+            return await storage.GetFileAsync("courtCoachData.xml");        //Rückgabe der existierenden Datei
         }
         public async Task<List<Session>> LoadData()
         {
-            var file = await getDataFile(); //bekommen xml file
-            var stream = (await file.OpenReadAsync()).AsStream(); //öffnen der datei zum lesen
+            var file = await getDataFile();
+            var stream = (await file.OpenReadAsync()).AsStream();           //öffnen der Datei zum lesen
             try
             {
-                XmlReader reader = XmlReader.Create(stream); // bauen eines xmlreaders
-                return ((SessionData)sr.Deserialize(reader)).Sessions; //deserialisieren und Rückgabe
+                XmlReader reader = XmlReader.Create(stream);                //Xml-reader für Datei erstellen
+                return ((SessionData)sr.Deserialize(reader)).Sessions;      //deserialisieren und Rückgabe
             }
             catch
             {
-                return new List<Session>();//wenn fehler rückgabe von leerer liste
+                return new List<Session>();                                 //wenn Fehler Rückgabe von leerer Liste
             }
         }
     }
